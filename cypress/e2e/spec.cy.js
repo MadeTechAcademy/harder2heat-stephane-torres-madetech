7465a586707d7a9accd5075c65a8d787d0b0d688
Stephane Torres <stephane.torres@madetech.com>
2024-07-26 16:33:26 +0100

    failing cypress test regarding the number of properties on the page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,14 @@ describe('template spec', () => {
     cy.visit('http://localhost:5000')
   })
 })
+
+describe("The correct information for each property is displayed", () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5000')
+  })
+  it("has the correct number of properties on the screen", () => {
+
+    cy.get("#properties").contains("4")
+
+  })
+})
