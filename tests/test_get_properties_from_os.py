dc5f417572d3e57124d2f153e854b5001721fc0c
Stephane Torres <stephane.torres@madetech.com>
2024-07-26 15:21:48 +0100

    GREEN: test sample properties is has expected length

--- tests/test_get_properties_from_os.py
+++ tests/test_get_properties_from_os.py
@@ -1,2 +1,14 @@
+import json
+from src.utils import get_properties_from_os
+
+
 def test_tests_are_running():
     assert True
+
+
+with open('properties.json') as json_properties:
+   data = json.load(json_properties)
+   properties = get_properties_from_os(data)
+
+def test_length_of_sample_properties_is_four():
+    assert len(properties) == 4
