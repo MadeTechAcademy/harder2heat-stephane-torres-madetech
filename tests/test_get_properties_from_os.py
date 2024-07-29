8f96b980e4cb17f1391633dfb4beda4c30d02c96
Stephane Torres <stephane.torres@madetech.com>
2024-07-29 09:19:26 +0100

    length of properties no longer hard coded in tests

--- tests/test_get_properties_from_os.py
+++ tests/test_get_properties_from_os.py
@@ -1,16 +1,18 @@
 import json
 from src.utils import get_properties_from_os
 
-EXPECTED_SAMPLE_LENGTH = 4
+with open('properties.json') as json_properties:
+   data = json.load(json_properties)
+   properties = get_properties_from_os(data)
+
+EXPECTED_SAMPLE_LENGTH = len(properties)
 
 
 def test_tests_are_running():
     assert True
 
 
-with open('properties.json') as json_properties:
-   data = json.load(json_properties)
-   properties = get_properties_from_os(data)
+
 
 def test_length_of_sample_properties_is_as_expected():
     assert len(properties) == EXPECTED_SAMPLE_LENGTH
