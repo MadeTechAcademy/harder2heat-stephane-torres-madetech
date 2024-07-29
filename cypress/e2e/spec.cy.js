74b402f98611d6c37eac78690b4f423c962b0fe0
Stephane Torres <stephane.torres@madetech.com>
2024-07-29 09:09:52 +0100

    GREEN: page contains a p tag with the correcet number of properties

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,9 +8,9 @@ describe("The correct information for each property is displayed", () => {
   beforeEach(() => {
     cy.visit('http://localhost:5000')
   })
-  it("has the correct number of properties on the screen", () => {
+  it("has the correct number of properties displayed on the screen", () => {
 
-    cy.get("#properties").contains("4")
+    cy.get("p").contains("4")
 
   })
 })
