c6d994e4212e90131fce68175c3930afd2ca028a
Stephane Torres <stephane.torres@madetech.com>
2024-08-05 15:47:59 +0100

    add a bit of error handling to attribute factory

--- src/attribute_factory.py
+++ src/attribute_factory.py
@@ -1,2 +1,5 @@
 def attribute_factory(desired_attribute: str, building_properties: dict) -> dict:
-    return {desired_attribute: building_properties.get(desired_attribute)}
+    value = building_properties.get(desired_attribute, False)
+    if value == False:
+        raise AttributeError(f'Attribute {desired_attribute} not found')
+    return {desired_attribute: value}
--- tests/test_attribute_factory.py
+++ tests/test_attribute_factory.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 from src.attribute_factory import attribute_factory
 
 
@@ -14,3 +15,8 @@ def test_attribute_factory_returns_dict():
 
 def test_attribute_factory_returns_desired_attribute_osid():
     assert attribute_factory("osid", buildings_attributes[0]) == {"osid": buildings_attributes[0]["osid"]}
+
+def test_error_is_thrown_when_attribute_not_found():
+
+    with pytest.raises(AttributeError):
+        attribute_factory("area", buildings_attributes[0])
