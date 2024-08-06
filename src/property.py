d03ea66788268135f898c90649456f68926dfd83
Stephane Torres <stephane.torres@madetech.com>
2024-08-06 13:31:00 +0100

    removal of unused constants in property.py

--- src/property.py
+++ src/property.py
@@ -1,8 +1,3 @@
-MINIMUM_EPC_RATING = "C"
-MINIMUM_FAILING_AGE = 1959
-COLD_CONNECTIVITY = "Standalone"
-WARM_MATERIALS = ["Brick Or Block Or Stone", "Concrete"]
-
 class Property():
     def __init__(self, uprn):
         self.uprn = uprn
