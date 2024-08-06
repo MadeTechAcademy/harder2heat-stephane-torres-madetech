f40b766e9611bf2e4f3a9f9671ec72a34ed1ab0d
Stephane Torres <stephane.torres@madetech.com>
2024-08-06 16:28:39 +0100

    test:GREEN building has coordinates attribute, little bit funny with how now attribute will be in a list, maybe should revert?...

--- src/property.py
+++ src/property.py
@@ -16,8 +16,4 @@
 class Property():
     def __init__(self, uprn):
         self.uprn = uprn
-        self.connectivity = ''
-        self.coordinates = []
-        self.osid = ''
-        self.date_age_last_updated = ''
-        self.area_m2 = 0
+
