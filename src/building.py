3b1f6cbcffa4a450d6c8d378939decb57802c931
Stephane Torres <stephane.torres@madetech.com>
2024-08-07 11:09:59 +0100

    test:GREEN, building contains a list of properties, but now not very resuable and a little bit stinky

--- src/building.py
+++ src/building.py
@@ -6,12 +6,16 @@ class Building:
     def __init__(self, coordinates, attributes):
         self.coordinates = coordinates
         self.attributes = attributes
-        self.properties = self.set_properties()
+        self.properties: list[Property] = []
+        self.get_properties_from_attributes()
 
 
-    def set_properties(self):
+    def get_properties_from_attributes(self):
         for attribute in self.attributes:
             for key, value in attribute.items():
                 if key == "uprnreference":
                     for property in attribute.get("uprnreference"):
                         self.properties.append(Property(property['uprn']))
+
+        # return self.properties
+# THIS IS STINKY
