6002f1ebac40c45c506ea97e1635a37bb7394ff6
Stephane Torres <stephane.torres@madetech.com>
2024-08-09 11:20:46 +0100

    remove todos from property

--- src/property.py
+++ src/property.py
@@ -1,16 +1,3 @@
-# TODO: Exploring composition, data coming in is of buildings that HAVE properties,
-#  do we want a building class? should property only be a data holding class?
-#  do want handlers? controllers? what do want to direct/control/manage?
-#  Building has individual properties/residences that only have uprns
-#  building has a list of different attributes, area, coords, osid, date etc, these are in "properties" obj within the data
-
-
-# TODO currently property gets its data from a nested obj, inherits
-
-# TODO create a building class, this has it's own id, and is made up of a property class and a attributes class
-#   attribute => name, value, any point of this as it'll be essentially just a dict
-
-
 
 
 class Property():
